77bce77f8cecf4cbf725bad3915e03ef80ff88c8
Thor Merlin Lervik <rohtgnilk@gmail.com>
2020-07-02 16:33:34 +0200

    Improve intro out transition

--- shaders/1593698869177.glsl
+++ shaders/1593698869177.glsl
@@ -5,15 +5,18 @@ vec4 pixel(vec2 p) {
     p -= 0.5;
     p.x *= resolution.x / resolution.y;
 
-    // return vec4(time - 13.);
-
     p.y += sin(p.x);
     p.y += (sin(p.x * 5.)) * .15;
 
-    if (abs(p.y) < (1. - (time - 12.7) * 0.5) * 1.65) {
+    p.y += texture(channel1, q).r * 0.6;
+
+    if (abs(p.y) < (1. - (time - 12.7) * 0.25) * 1.65) {
         return texture(channel1, q);
     }
 
     q.x -= 0.001;
-    return (texture(channel0, q) + 0.01) * vec4(.98, .9, 1.5, 0.);
+    q.y *= 0.98;
+    q.y += 0.01;
+
+    return (texture(channel0, q) + 0.01) * vec4(.98, .9 - tan(p.x * 0.7), 1.5, 0.);
 }
