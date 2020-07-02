13a3a2fa695d50256c7c61886e89bf69fc24cf29
Thor Merlin Lervik <rohtgnilk@gmail.com>
2020-07-02 16:49:34 +0200

    Add transition between wave scene and intr out transition

---
+++ shaders/1593701124975.glsl
@@ -0,0 +1,10 @@
+vec4 pixel(vec2 p) {
+    p /= resolution;
+    vec2 q = p;
+    p -= .5;
+    p.x *= resolution.x / resolution.y;
+
+    float trans = (time - 20.5) * .5;
+
+    return mix(texture(channel1, q), texture(channel2, q), trans);
+}
