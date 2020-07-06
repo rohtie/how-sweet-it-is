7ca4283b33cb6ca4b74e8f33572cc0881dee40aa
Thor Merlin Lervik <rohtgnilk@gmail.com>
2020-07-06 16:12:16 +0200

    Improve ending again

--- shaders/1591895887463.glsl
+++ shaders/1591895887463.glsl
@@ -6,9 +6,9 @@ vec4 pixel(vec2 p) {
 
     p.y += tan(p.x * 1.9) * mod(-time, 5.);
 
-    p.x += sin(p.y * 20. + time * 4. + sin(time + p.y * 20. + cos(p.y * 7. + time *15. + p.x * 20.)));
+    p.x += sin(p.y * 20. + time * 44. + sin(time + p.y * 20. + cos(p.y * 7. + time *15. + p.x * 20.)));
 
     float r = length(p) - .5;
 
-    return texture(channel0, q) / vec4(r, r*.5 * p.x * .05, p.x * 0.7, 0.);
+    return texture(channel0, q) * vec4(r, r*.5 * p.x * .05, p.x * 0.7, 0.) * .1;
 }
