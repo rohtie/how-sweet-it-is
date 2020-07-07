4b4ac44ee90c4ca80c0a7989ae1171ee5b3131e0
Thor Merlin Lervik <rohtgnilk@gmail.com>
2020-07-07 17:02:18 +0200

    Fix intro transition feedback shaders

--- shaders/1593698869177.glsl
+++ shaders/1593698869177.glsl
@@ -8,15 +8,23 @@ vec4 pixel(vec2 p) {
     p.y += sin(p.x);
     p.y += (sin(p.x * 5.)) * .15;
 
-    p.y += texture(channel1, q).r * 0.6;
+    vec4 intro = texture(channel1, q);
+
+    p.y += intro.r * 0.6;
 
     if (abs(p.y) < (1. - ((time + tan(p.x * 1. + time * 30.) * .2) - 12.7) * 0.18) * 1.65) {
-        return texture(channel1, q) * tan(time * 37.);
+        return intro * tan(time * 37.);
     }
 
     q.x -= 0.001;
     q.y *= 0.98;
     q.y += 0.01;
 
-    return (texture(channel0, q) + 0.01) * vec4(.98, .9 - tan(p.x * 0.7), 1.5, 0.);
+    vec4 self = texture(channel0, q);
+
+    if (time > 20.55) {
+        self = texture(channel2, q);
+    }
+
+    return (self + 0.01) * vec4(.98, .9 - tan(p.x * 0.7), 1.5, 0.);
 }
